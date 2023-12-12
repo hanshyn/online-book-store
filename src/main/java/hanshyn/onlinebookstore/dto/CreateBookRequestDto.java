c5c5058ccd6298950f305b0c7782fb565f3c2f26
Dmytro Hanshyn <di.hanshyn@gmail.com>
2023-12-12 14:00:26 +0200

    fixed: Added check: @NotEmpty field author, field isbn @ISBN, field description limit size

--- src/main/java/hanshyn/onlinebookstore/dto/CreateBookRequestDto.java
+++ src/main/java/hanshyn/onlinebookstore/dto/CreateBookRequestDto.java
@@ -1,19 +1,25 @@
 package hanshyn.onlinebookstore.dto;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Data;
+import org.hibernate.validator.constraints.ISBN;
 
 @Data
 public class CreateBookRequestDto {
     @NotNull
+    @NotEmpty
     private String title;
 
     @NotNull
+    @NotEmpty
     private String author;
 
     @NotNull
+    @ISBN
     private String isbn;
 
     @NotNull
@@ -21,6 +27,7 @@ public class CreateBookRequestDto {
     private BigDecimal price;
 
     @NotNull
+    @Size(min = 10, max = 200)
     private String description;
 
     @NotNull
