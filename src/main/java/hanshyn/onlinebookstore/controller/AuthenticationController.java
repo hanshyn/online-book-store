5e278a85dcf70101dcf4dbd71fdceb0f3bcc59cb
hanshyn <128403315+hanshyn@users.noreply.github.com>
2024-01-03 19:29:59 +0200

    Book store jwt (#9)
    * Added dependency jjwt
    
    * Added implement JWT support

--- src/main/java/hanshyn/onlinebookstore/controller/AuthenticationController.java
+++ src/main/java/hanshyn/onlinebookstore/controller/AuthenticationController.java
@@ -1,9 +1,13 @@
 package hanshyn.onlinebookstore.controller;
 
+import hanshyn.onlinebookstore.dto.user.UserLoginRequestDto;
+import hanshyn.onlinebookstore.dto.user.UserLoginResponseDto;
 import hanshyn.onlinebookstore.dto.user.UserRegistrationRequestDto;
 import hanshyn.onlinebookstore.dto.user.UserResponseDto;
 import hanshyn.onlinebookstore.exception.RegistrationException;
+import hanshyn.onlinebookstore.security.AuthenticationService;
 import hanshyn.onlinebookstore.service.user.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,9 +19,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthenticationController {
     private final UserService userService;
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/login")
+    public UserLoginResponseDto login(@RequestBody @Valid UserLoginRequestDto requestDto) {
+        return authenticationService.authenticate(requestDto);
+    }
 
     @PostMapping("/registration")
-    public UserResponseDto register(@RequestBody UserRegistrationRequestDto requestDto)
+    public UserResponseDto register(@RequestBody @Valid UserRegistrationRequestDto requestDto)
             throws RegistrationException {
         return userService.register(requestDto);
     }
