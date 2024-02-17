93b950f706cc0520bdbd2a805f93982b9f3301e7
Dmytro Hanshyn <di.hanshyn@gmail.com>
2024-02-17 15:27:08 +0200

    Deleted OrderNotFoundException

--- src/main/java/hanshyn/onlinebookstore/exception/OrderNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package hanshyn.onlinebookstore.exception;
-
-public class OrderNotFoundException extends RuntimeException {
-    public OrderNotFoundException(String message) {
-        super(message);
-    }
-}
